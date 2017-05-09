209a1c25c77bbd0b563b2e909f538924d1029593
carlosylourdes <carlosylourdes@carlosylourdes>
2017-05-09 21:42:13 +0200

    Todas las funciones básicas (add, edit, delete, refresh y export) funcionando, además de la paginación.

--- jquery1/src/controller/DeveloperController.java
+++ jquery1/src/controller/DeveloperController.java
@@ -10,68 +10,76 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import model.DeveloperImpl;
+import service.ServiceDeveloperImpl;
+import utils.Tools.Operation;
+
 import com.google.gson.Gson;
 
-import model.DeveloperImpl;
 import dao.DeveloperDAOImpl;
 
 @WebServlet("/DeveloperController")
 public class DeveloperController extends HttpServlet {
+	
 	private static final long serialVersionUID = 1L;
-
+	ServiceDeveloperImpl servicioDeveloper;
 	/**
 	 * Default constructor. 
 	 */
 	public DeveloperController() {
-		// TODO Auto-generated constructor stub
+		servicioDeveloper = new ServiceDeveloperImpl();
+		
 	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-		String developerId = request.getParameter("id");
-		
-		DeveloperDAOImpl developerDAO = new DeveloperDAOImpl();
-		ArrayList<DeveloperImpl> developers = developerDAO.getDeveloper(Integer.parseInt(developerId));
-		developerDAO.deleteDeveloper(developers.get(0));
-		
-		PrintWriter out = response.getWriter();
-		response.setContentType("application/json");
-		String json = new Gson().toJson(developers);
-		out.println(json);
-		out.close();
-
+		accion(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//doPost1();
-		doPost2(request, response);
-	}
-	protected void doPost1(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		String developerId = request.getParameter("fullname");
-
-		PrintWriter out = response.getWriter();
-		response.setContentType("text/plain");
-
-		DeveloperDAOImpl developerDAO = new DeveloperDAOImpl();
-		ArrayList<DeveloperImpl> developers = developerDAO.getDeveloper(Integer.parseInt(developerId));
-		if (!developers.isEmpty())
-			out.println(developers.get(0).getEntityDescription());
-		else
-			out.println(" Developer no encontrado");
-		out.close();
+		accion(request, response);
 	}
 	
-	protected void doPost2(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void accion(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		DeveloperDAOImpl developerDAO = new DeveloperDAOImpl();
-		ArrayList<DeveloperImpl> developers = developerDAO.listDevelopers();
+		String oper= request.getParameter("oper");
+		
+		if (oper== null) oper="search";
+		
+		switch (Operation.valueOf(oper)) {
+		case search:
+			buscarDevelopers(request, response);
+			break;
+		case add:
+			agregarDeveloper(request, response);
+			break;
+		case edit:
+			editarDeveloper(request, response);
+			break;
+		case del:
+			eliminarDeveloper(request, response);
+			break;
+		case export:
+			exportarCSV(request, response);
+			break;
+		default:
+			buscarDevelopers(request, response);
+			break;
+		}
+		
+	}
+	
+	protected void buscarDevelopers(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException { 
+		
+		//DeveloperDAOImpl developerDAO = new DeveloperDAOImpl();
+		//ArrayList<DeveloperImpl> developers = developerDAO.listDevelopers();
+		
+		ArrayList<DeveloperImpl> developers = servicioDeveloper.buscarDevelopers();
 		
 		PrintWriter out = response.getWriter();
 		response.setContentType("application/json");
@@ -81,5 +89,83 @@ public class DeveloperController extends HttpServlet {
 
 	}
 
+	protected void agregarDeveloper(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String devId = request.getParameter("id");
+		String devFirstName = request.getParameter("firstName");
+		String devLastName = request.getParameter("lastName");
+		String devVelocity = request.getParameter("velocity");
+		
+		//DeveloperDAOImpl developerDAO = new DeveloperDAOImpl();
+		//DeveloperImpl dev = new DeveloperImpl();
+		//dev.setFirstName(devFirstName);
+		//dev.setLastName(devLastName);
+		//dev.setVelocity(Integer.parseInt(devVelocity));
+		//developerDAO.saveDeveloper(dev);
+		
+		servicioDeveloper.agregarDeveloper(devFirstName, devLastName, Integer.parseInt(devVelocity));
+		
+		ArrayList<DeveloperImpl> developers = servicioDeveloper.buscarDevelopers();
+		
+		PrintWriter out = response.getWriter();
+		response.setContentType("application/json");
+		String json = new Gson().toJson(developers);
+		out.println(json);
+		out.close();
+		
+	}
 
+	protected void editarDeveloper(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String devId = request.getParameter("id");
+		String devFirstName = request.getParameter("firstName");
+		String devLastName = request.getParameter("lastName");
+		String devVelocity = request.getParameter("velocity");
+		
+		//DeveloperDAOImpl developerDAO = new DeveloperDAOImpl();
+		//ArrayList<DeveloperImpl> developers = developerDAO.getDeveloper(Integer.parseInt(devId));
+		//DeveloperImpl dev = developers.get(0);
+		//dev.setFirstName(devFirstName);
+		//dev.setLastName(devLastName);
+		//dev.setVelocity(Integer.parseInt(devVelocity));
+		//developerDAO.updateDeveloper(dev);
+		
+		servicioDeveloper.editarDeveloper(Integer.parseInt(devId), devFirstName, devLastName, Integer.parseInt(devVelocity));
+		
+		ArrayList<DeveloperImpl> developers = servicioDeveloper.buscarDevelopers();
+		
+		PrintWriter out = response.getWriter();
+		response.setContentType("application/json");
+		String json = new Gson().toJson(developers);
+		out.println(json);
+		out.close();
+		
+	}
+
+	protected void eliminarDeveloper(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String developerId = request.getParameter("id");
+		
+		//DeveloperDAOImpl developerDAO = new DeveloperDAOImpl();
+		//ArrayList<DeveloperImpl> developers = developerDAO.getDeveloper(Integer.parseInt(developerId));
+		//developerDAO.deleteDeveloper(developers.get(0));
+		servicioDeveloper.eliminarDeveloper(Integer.parseInt(developerId));
+		
+		ArrayList<DeveloperImpl> developers = servicioDeveloper.buscarDevelopers();
+		
+		PrintWriter out = response.getWriter();
+		response.setContentType("application/json");
+		String json = new Gson().toJson(developers);
+		out.println(json);
+		out.close();
+	}
+
+	protected void exportarCSV(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/plain");
+		out.println(servicioDeveloper.exportarCSV());
+		out.close();
+		
+	}
 }
--- jquery1/src/service/ServiceDeveloperImpl.java
+++ jquery1/src/service/ServiceDeveloperImpl.java
@@ -1,5 +1,78 @@
 package service;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.DeveloperImpl;
+
+import com.csvreader.CsvWriter;
+import com.google.gson.Gson;
+
+import dao.DeveloperDAOImpl;
+
 public class ServiceDeveloperImpl {
 
+	public static String SALTO_LINEA = System.getProperty("line.separator");
+	
+	public ArrayList<DeveloperImpl> buscarDevelopers() {
+		
+		DeveloperDAOImpl developerDAO = new DeveloperDAOImpl();
+		ArrayList<DeveloperImpl> developers = developerDAO.listDevelopers();
+		
+		return developers;
+	}
+
+	public DeveloperImpl agregarDeveloper(String firstName, String lastName, Integer velocity) {
+		
+		DeveloperDAOImpl developerDAO = new DeveloperDAOImpl();
+		DeveloperImpl dev = new DeveloperImpl();
+		dev.setFirstName(firstName);
+		dev.setLastName(lastName);
+		dev.setVelocity(velocity);
+		developerDAO.saveDeveloper(dev);
+		
+		return dev;
+	}
+
+	public DeveloperImpl editarDeveloper(Integer id, String firstName, String lastName, Integer velocity) {
+
+		DeveloperDAOImpl developerDAO = new DeveloperDAOImpl();
+		ArrayList<DeveloperImpl> developers = developerDAO.getDeveloper(id);
+		DeveloperImpl dev = developers.get(0);
+		
+		dev.setFirstName(firstName);
+		dev.setLastName(lastName);
+		dev.setVelocity(velocity);
+		developerDAO.updateDeveloper(dev);
+		
+		return dev;
+	}
+
+	public void eliminarDeveloper(Integer id) {
+		
+		DeveloperDAOImpl developerDAO = new DeveloperDAOImpl();
+		ArrayList<DeveloperImpl> developers = developerDAO.getDeveloper(id);
+		developerDAO.deleteDeveloper(developers.get(0));
+	}
+	
+	public String exportarCSV() {
+		
+		String fichero ="";
+		ArrayList<DeveloperImpl> listaDevelopers = buscarDevelopers();
+		 
+        for(DeveloperImpl dev : listaDevelopers){
+        	fichero+="\""+dev.getId().toString()+"\",";
+        	fichero+="\""+dev.getFirstName()+"\",";
+        	fichero+="\""+dev.getLastName()+"\",";
+        	fichero+="\""+dev.getVelocity().toString()+"\""+SALTO_LINEA;
+        }
+        
+		return fichero;
+	}
 }
